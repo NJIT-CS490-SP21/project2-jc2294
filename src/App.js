eb484fa156f74cded60726f2d631327b858083c0
juhichaudhari <juhichaudhari@gmail.com>
2021-02-28 14:43:42 +0000

    saving usernames on server side into a dictionary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-//import { ListItem } from './ListItem.js';
+import { ListItem } from './ListItem.js';
 import { useState, useRef, useEffect } from 'react';
 import io from 'socket.io-client';
 import { Board } from './Board.js';
 import { Login } from './Login.js';
 
 
+
 const socket = io(); // Connects to socket connection
 
 function App() {
   //user longs in
-  const [usernames, setUsername] = useState([]); // State variable, list of users
+
+  const [usernames, setUsername] = useState(""); // State variable to store usernames
   const [loggedIn, setLoggedIn] = useState(false);  //initialize logged in status to fasle
   
-  let username;
-  const loginOnClick = (username)=> {
-    username = username;
-    setUsername(username);
-    setLoggedIn(!loggedIn)  //set loogded in to true
-    socket.emit('login',{setUsername:username});
+  let userInput;
+  const loginOnClick = (userInput)=> {
+    if(userInput != null){
+      console.log(userInput)
+      
+      //setUsername(prevUsernames => [...prevUsernames, usernames]);
+
+      setUsername(userInput);
+      
+      //setLoggedIn(!loggedIn)  //set loogded in to opposite
+      setLoggedIn((prevLog) => {
+        return !prevLog;
+      });
+      
+      socket.emit('login',{username:userInput});
+    };
   }
   
   useEffect(() => {
@@ -30,7 +42,9 @@ function App() {
       console.log(login);
       // If the server sclick (on behalf of another client), then we
       // add it to the list of messages to render it on the UI.
-      setUsername([...login.setUsername]);
+      
+      setUsername(prevUsernames => [...prevUsernames, usernames])
+      //setUsernamesList([...login.setUsername]); this
     });
   }, []);
 
@@ -38,6 +52,10 @@ function App() {
   if(loggedIn && usernames != ""){
     return (
       <div>
+        <h3>Player X:</h3>
+        <h3>Player O:</h3>
+        <ul>
+        </ul>
         <Board/>
       </div>
       
