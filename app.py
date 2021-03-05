fb40851c6ea992498aaa723bbb6f09448e197114
juhichaudhari <juhichaudhari@gmail.com>
2021-03-05 03:28:23 +0000

    setting up database and added models.py. server is able to add new users to the database when a user logs in

--- app.py
+++ app.py
@@ -2,8 +2,24 @@ import os
 from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv()) # This is to load your env variables from .env
 
 app = Flask(__name__, static_folder='./build/static')
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+import models
+db.create_all()
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -57,6 +73,18 @@ def on_login(data): # data is whatever arg you pass in your emit call on client
         users["spectators"].append(currentUser)
 
     print(users)
+    
+    #working with databsse
+    new_user = models.Person(username=data['username'], score=100)
+    db.session.add(new_user)
+    db.session.commit()
+    all_people = models.Person.query.all()
+    userss = []
+    for person in all_people:
+        userss.append(person.username)
+        
+    print(users)
+    
     socketio.emit('login', users, broadcast=True, include_self=False)
 
 @socketio.on('click')
@@ -73,9 +101,12 @@ def on_reset(data): # data is whatever arg you pass in your emit call on client
     # the client that emmitted the event that triggered this function
     socketio.emit('reset',  data, broadcast=True, include_self=False)
 
+
+# Note we need to add this line so we can import app in the python shell
+if __name__ == "__main__":
 # Note that we don't call app.run anymore. We call socketio.run with app arg
-socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-)
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    )
